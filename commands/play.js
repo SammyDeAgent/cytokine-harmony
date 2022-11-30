3c3a561751a29f2333fd2e5542118deb7c1f8f5f
SammyDeAgent <rimamotostheking@gmail.com>
2022-12-01 01:45:20 +0800

    Changed ytdl-core to play-dl library

--- commands/play.js
+++ commands/play.js
@@ -5,10 +5,13 @@ const {
   joinVoiceChannel,
   createAudioPlayer,
   createAudioResource,
+  getVoiceConnection,
+  NoSubscriberBehavior,
   AudioPlayerStatus,
   generateDependencyReport
 } = require('@discordjs/voice');
-const ytdl = require('ytdl-core');
+// const ytdl = require('ytdl-core');
+const ytdl = require('play-dl');
 const ytSearch = require('yt-search');
 
 module.exports = {
@@ -23,18 +26,24 @@ module.exports = {
     ),
   async execute(interaction) {
 
-    const voiceChannel = interaction.guild.members.cache.get(interaction.member.user.id).voice.channel;
+    await interaction.deferReply();
+
+    const voiceChannel = await interaction.guild.members.cache.get(interaction.member.user.id).voice.channel;
     if (!voiceChannel) {
       await interaction.reply("```You need to be in a Channel to execute this command.```");
     } else {
 
       // Joins the user's channel
-      const connection = joinVoiceChannel({
-        channelId: voiceChannel.id,
-        guildId: interaction.guildId,
-        adapterCreator: interaction.guild.voiceAdapterCreator,
-      });
-
+      var connection = getVoiceConnection(interaction.guildId);
+      
+      if(!connection) {
+        connection = joinVoiceChannel({
+          channelId: voiceChannel.id,
+          guildId: interaction.guildId,
+          adapterCreator: interaction.guild.voiceAdapterCreator,
+        });
+      }
+      
       // Check for "CONNECT" and "SPEAK" permissions
 
       const videoFinder = async (query) => {
@@ -42,25 +51,40 @@ module.exports = {
         return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
       }
 
-      const video = await videoFinder(interaction.options.getString("query"));
-      if (video) {
-        const stream = ytdl(video.url, {
-          filter: 'audioonly',
-          opusEncoded: true,
-          dlChunkSize: 0,
-          highWaterMark: 1 << 25,
-        });
-        const player = createAudioPlayer();
+      const query = await interaction.options.getString("query")
+      const video = await videoFinder(query);
 
-        let resource = createAudioResource(stream, {
-          inlineVolume: true
+      if (video) {
+        // const stream = ytdl(video.url, {
+        //   fmt: "mp3",
+        //   highWaterMark: 1 << 62,
+        //   liveBuffer: 1 << 62,
+        //   dlChunkSize: 0, //disabling chunking is recommended in discord bot
+        //   bitrate: 128,
+        //   quality: "lowestaudio",
+        // });
+
+        const stream = await ytdl.stream(video.url);
+
+        let resource = createAudioResource(stream.stream, {
+          inlineVolume: true,
+          inputType: stream.type,
         });
         resource.volume.setVolume(0.1);
+
+        const player = createAudioPlayer({
+          behaviors: {
+            noSubscriber: NoSubscriberBehavior.Play
+          }
+        });
 
         connection.subscribe(player);
         player.play(resource);
 
-        await interaction.reply(`Playing - ${video.url}`);
+        // await interaction.reply(`Playing - ${video.url}`);
+        await interaction.editReply({
+          content: `Playing - ${video.url}`
+        });
       }else {
         await interaction.reply("```Could not find any search results.```");
       }
