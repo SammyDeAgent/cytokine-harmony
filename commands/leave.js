6662b46991a51296b6df53bda88fe80c56f7759e
SammyDeAgent <rimamotostheking@gmail.com>
2022-11-30 14:47:31 +0800

    Implemented leave function

---
+++ commands/leave.js
@@ -0,0 +1,40 @@
+const {
+  SlashCommandBuilder
+} = require('@discordjs/builders');
+const {
+  joinVoiceChannel,
+  getVoiceConnection,
+  createAudioPlayer,
+  createAudioResource,
+  AudioPlayerStatus,
+  generateDependencyReport
+} = require('@discordjs/voice');
+const ytdl = require('ytdl-core');
+const ytSearch = require('yt-search');
+
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('leave')
+    .setDescription(
+      "Leave the voice channel and terminate connection"
+    ),
+  async execute(interaction) {
+
+    const voiceChannel = interaction.guild.members.cache.get(interaction.member.user.id).voice.channel;
+    if (!voiceChannel) {
+      await interaction.reply("```You need to be in a Channel to execute this command.```");
+    } else {
+
+      // Joins the user's channel
+      const connection = getVoiceConnection(interaction.guildId);
+
+      if(!connection) {
+        await interaction.reply("```Harmony is currently not in any channel.```");
+      } else {
+        connection.destroy();
+        await interaction.reply("```Connection Terminated.```");
+      }
+
+    }
+  }
+};
